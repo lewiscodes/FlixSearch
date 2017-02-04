e98271dde20d2564fed052a4369e7f7766a85886
Lewis <lewis.turner@Lewiss-Air.lan>
2017-02-04 07:53:25 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import {Router, browserHistory } from 'react-router';
+import reducers from './reducers';
+import routes from './routes';
+import promise from 'redux-promise';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const createStoreWithMiddleware = createStore(reducers, composeEnhancers(
+  applyMiddleware(promise)
+));
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware}>
+    <Router history={browserHistory} routes={routes}/>
+  </Provider>
+  , document.querySelector('.container'));
